a0a2dddbe0d9897aa0f89e1112d9f82cbb908470
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-19 20:09:17 -0400

    added bullet points, fixed formatting in about.swift

--- smartbasket/about.swift
+++ smartbasket/about.swift
@@ -14,6 +14,11 @@ struct AboutUsView: View{
                         
                         Text("Welcome to Smart Basket - Your Ultimate Shopping Companion!")
                             .font(.body)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("At Smart Basket, we believe that shopping should be simple, organized, and enjoyable. Out app is designed to help you manage your shopping lists effortlessly, track your expenses, and categorize your items with ease. Whether you're planning weekly groceries or preparing for a special event, Smart Basket has your covered.")
+                            .font(.body)
                             .multilineTextAlignment(.leading)
                         
                         Divider()
@@ -21,6 +26,7 @@ struct AboutUsView: View{
                         Text("Our Mission")
                             .font(.headline)
                             .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         Text("To simplify shopping for everyone by offering an intuitive and smart way to create and manage shopping lists. We aim to save you time, reduce the hassle, and help you stay on budget.")
                             .font(.body)
                             .multilineTextAlignment(.leading)
@@ -30,19 +36,43 @@ struct AboutUsView: View{
                         Text("Why Choose Smart Basket?")
                             .font(.headline)
                             .fontWeight(.semibold)
-                    
-                        
-                        Text("Total Cost Tracking: Get real-time calculation of your shopping costs, including taxes.")
-                        Text("Custom Categories: Personalize your lists with custom categories and items.")
-                        Text("User-Friendly Design: Simple, clean, and easy to navigate.")
-                            .font(.body)
-                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        VStack(alignment: .leading, spacing: 5){
+                            HStack{
+                                Text("•")
+                                    .font(.system(size:30))
+                                Text("Organized Shopping: Easily categorize and organize your items by type.")
+                                    .font(.body)
+                                    .multilineTextAlignment(.leading)
+                            }
+                            HStack{
+                                Text("•")
+                                    . font(.system(size:30))
+                                Text("Total Cost Tracking: Get real-time calculation of your shopping costs, including taxes.")
+                                    .font(.body)
+                                    .multilineTextAlignment(.leading)
+                            }
+                            HStack{
+                                Text("•")
+                                    . font(.system(size:30))
+                                Text("Custom Categories: Personalize your lists with custom categories and items.")
+                                    .font(.body)
+                                    .multilineTextAlignment(.leading)
+                            }
+                            HStack{
+                                Text("•")
+                                    . font(.system(size:30))
+                                Text("User-Friendly Design: Simple, clean, and easy to navigate.")
+                                    .font(.body)
+                                    .multilineTextAlignment(.leading)
+                            }                        }
                         
                         Divider()
                         
                         Text("Our Vision")
                             .font(.headline)
                             .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         Text("We strive to become the most trusted shopping assistant, helping people all around the world organize their lives better.")
                             .font(.body)
                             .multilineTextAlignment(.leading)
@@ -52,6 +82,7 @@ struct AboutUsView: View{
                         Text("Meet Our Team:")
                             .font(.headline)
                             .fontWeight(.semibold)
+                         
                         Text("Mobinasadat Zargary")
                         Text("Kevin Lapointe")
                             .font(.body)
